b61a6b46c8308c2cdab5d42fbecbab390d18581b
CreativeMind786 <mirzayeejan@yahoo.com>
2022-11-28 17:56:32 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const controller = require( "./controllers/bookcontroller");
+// GET / - return the index.html file
+router.get("/" , controller.home);
+router.get("/index" , controller.home);
+router.get("/books" , controller.books);
+router.get("/search_books" , controller.search_books);
+router.use("/book" ,controller.book  );
+router.get("/insert" , controller.insert);
+// export
+module.exports = router;
