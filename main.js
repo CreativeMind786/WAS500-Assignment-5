2b3b9c3f3dea15a15aab84901dfa15ea2f167641
CreativeMind786 <117135397+CreativeMind786@users.noreply.github.com>
2022-12-02 16:46:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const controller = require( "./controllers/bookcontroller");
 // GET / - return the index.html file
 router.get("/" , controller.home);
 router.get("/index" , controller.home);
+router.get("/home" , controller.home);
+router.get("/booksList" , controller.books);
 router.get("/books" , controller.books);
+router.get("/books/:id" , controller.books);
 router.get("/search_books" , controller.search_books);
 router.use("/book" ,controller.book  );
 router.get("/insert" , controller.insert);
